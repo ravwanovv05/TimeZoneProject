3bd65c6b0217c246e76a82ad3c8e83f16fdbd015
ravwanovv05 <samidilloravshanov14@gmail.com>
2023-10-04 11:06:49 +0500

    change

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,13 @@
+from modeltranslation.admin import TranslationAdmin
 from django.contrib import admin
-from .models import ProductList
-# Register your models here.
+from .models import ProductList, Picture
 
 
-admin.site.register(ProductList)
+@admin.register(ProductList)
+class ProductListAdmin(TranslationAdmin):
+    pass
+
+
+admin.site.register(Picture)
+
 
