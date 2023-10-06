ef2164ed7e751daabfc889e05ca9122cd7a60816
ravwanovv05 <samidilloravshanov14@gmail.com>
2023-10-07 00:24:39 +0500

    change

--- main/middleware.py
+++ main/middleware.py
@@ -9,8 +9,8 @@ class IPRestrictedMiddleware:
     def __call__(self, request, *args, **kwargs):
         client_ip = request.META.get('REMOTE_ADDR')
         ip_address = [
-            # '127.0.0.1',
-            '10.10.3.108'
+            '127.0.0.1',
+            '10.10.3.108 '
         ]
         if client_ip not in ip_address and request.path.find('admin') != -1:
             return HttpResponseForbidden('Permission denied!')
