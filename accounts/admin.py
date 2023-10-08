25fa4b5f9f2bd6ad162ee72b27291b8f05d2a8e1
ravwanovv05 <samidilloravshanov14@gmail.com>
2023-10-08 18:02:38 +0500

    change

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserData
 
-# Register your models here.
+
+@admin.register(UserData)
+class UserDataAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'phone_number', 'last_login')
+    list_filter = ('username', 'first_name', 'last_name')
+    list_editable = ('first_name', 'last_name', 'phone_number')
