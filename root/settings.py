5783d8989bf8ae33ecd0f7a0b23dbf1633185807
ravwanovv05 <samidilloravshanov14@gmail.com>
2023-12-22 09:15:30 +0500

    change

--- root/settings.py
+++ root/settings.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'timezone_db',
-        'USER': 'postgres',
-        'PASSWORD': 1,
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'timezone_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 1,
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
